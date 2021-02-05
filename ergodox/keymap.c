1231a06045f9ccfbc600c069ced191bd5ddf30e8
Thomas Kobber Panum <thomas@panum.dk>
2021-02-05 12:23:54 +0100

    Updated layout

--- ergodox/keymap.c
+++ ergodox/keymap.c
@@ -9,7 +9,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BASE] = LAYOUT_ergodox_pretty(
 				 KC_GRAVE,       KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_LBRACKET,                                    KC_RBRACKET,    KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_BSLASH,
 				 KC_LCTRL,       KC_Q,           KC_W,           KC_F,           KC_P,           KC_G,           KC_EQUAL,                                       KC_MINUS,       KC_J,           KC_L,           KC_U,           KC_Y,           KC_SCOLON,      KC_LCTRL,
-				 LT(KC_LSHIFT,KC_ESC),      KC_A,           KC_R,           KC_S,           KC_T,           KC_D,                                                                           KC_H,           KC_N,           KC_E,           KC_I,           KC_O,            LT(KC_LSHIFT,KC_ESC),
+				 LSFT_T(KC_ESC),      KC_A,           KC_R,           KC_S,           KC_T,           KC_D,                                                                           KC_H,           KC_N,           KC_E,           KC_I,           KC_O,            LSFT_T(KC_ESC),
 				 KC_LGUI,        KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,           KC_TAB,                                         KC_TAB,         KC_K,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_LGUI,
 				 KC_HOME,        KC_END,         KC_LEFT,        KC_RALT,       MO(1),                                                                                                          MO(1),          KC_RALT,          KC_DOWN,        KC_LEFT,        KC_RIGHT,
 				 RESET,          MO(2),          KC_LALT,        KC_DELETE,
