f940d0059c30679e0f0474ed76a4d834bab05e98
Thomas Kobber Panum <thomas@panum.dk>
2021-02-08 12:18:26 +0100

    Removed config.h

--- config.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#define PERMISSIVE_HOLD
-#define IGNORE_MOD_TAP_INTERRUPT
-#define RETRO_TAPPING
--- kinesis/keymap.c
+++ kinesis/keymap.c
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
            KC_ESC ,KC_F1  ,KC_F2  ,KC_F3  ,KC_F4  ,KC_F5  ,KC_F6  ,KC_F7  ,KC_F8,
            KC_GRV,KC_1   ,KC_2   ,KC_3   ,KC_4   ,KC_5   ,
            KC_LCTL,KC_Q  ,KC_W   ,KC_F   ,KC_P   ,KC_G   ,
-           LSFT_T(KC_ESC), KC_A   ,KC_R   ,KC_S   ,KC_T   ,KC_D   ,
+           KC_LSFT, KC_A   ,KC_R   ,KC_S   ,KC_T   ,KC_D   ,
            KC_RGUI,KC_Z   ,KC_X   ,KC_C   ,KC_V   ,KC_B   ,
 	   KC_HOME,KC_END ,KC_RALT,MO(SYMB),
 	   KC_LCTL,KC_LALT,
@@ -44,9 +44,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	   KC_SPC,LT(NUMS,KC_TAB),KC_LALT ,
 
 	   KC_F9  ,KC_F10 ,KC_F11 ,KC_F12 ,KC_PSCR ,KC_SLCK  ,KC_PAUS, KC_FN0, RESET,
-	   KC_6   ,KC_7   ,KC_8   ,KC_9   ,KC_0   ,KC_MINS,
-	   KC_J   ,KC_L   ,KC_U   ,KC_Y   ,KC_SCLN,KC_RCTL,
-	   KC_H   ,KC_N   ,KC_E   ,KC_I   ,KC_O   ,LSFT_T(KC_ESC),
+	   KC_6   ,KC_7   ,KC_8   ,KC_9   ,KC_0   , KC_MINS,
+	   KC_J   ,KC_L   ,KC_U   ,KC_Y   ,KC_SCLN, KC_RCTL,
+	   KC_H   ,KC_N   ,KC_E   ,KC_I   ,KC_O   , KC_LSFT,
 	   KC_K   ,KC_M   ,KC_COMM,KC_DOT ,KC_SLSH,KC_RGUI,
 	   MO(SYMB),KC_RALT,KC_PGDN,KC_PGUP,
            KC_RGUI,KC_RCTL,
@@ -99,18 +99,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     )
 };
 
-void matrix_init_user(void) {
 
-}
-
-void matrix_scan_user(void) {
-
-}
+static uint16_t last_keycode = 0;
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+  case KC_LSFT:
+    if (!record->event.pressed && last_keycode == keycode) {
+      unregister_code(KC_LSFT);
+      register_code(KC_ESC);
+      unregister_code(KC_ESC);
+      return false;
+    }
+  }
+
+  last_keycode = keycode;
   return true;
-}
-
-void led_set_user(uint8_t usb_led) {
-
 }
