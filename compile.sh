297d1a37cca86d3e93322b82be9900b4cd73af97
Thomas Kobber Panum <thomas@panum.dk>
2024-04-09 14:07:48 +0200

    Added Vial variants for Kinesis boards

--- compile.sh
+++ compile.sh
@@ -35,18 +35,3 @@ for keyboard in kinesis ergodox; do
     short_build_keyboard=$(echo $build_keyboard | cut -d'/' -f1)
     mv .build/${short_build_keyboard}_${variant_keyboard}_tpanum.hex ../${keyboard}_tpanum.hex
 done
-
-
-# BUILD_FOLDER=$(pwd)/.build
-# mkdir -p $BUILD_FOLDER
-
-# docker run --rm \
-#        -v $(pwd)/ergodox:/qmk_firmware/keyboards/ergodox_ez/keymaps/default \
-#        -v $(pwd)/kinesis:/qmk_firmware/keyboards/kinesis/keymaps/default \
-#        -v $BUILD_FOLDER:/qmk_firmware/.build \
-#        ghcr.io/qmk/qmk_cli \
-#        make "all"
-#        # bash -c "qmk compile -kb ergodox_ez -km default && qmk compile -kb kinesis/stapelberg -km default"
-
-# mv $BUILD_FOLDER/ergodox_ez_default.hex ergodox_ez_tpanum.hex
-# mv $BUILD_FOLDER/kinesis_stapelberg_default.hex kinesis_stapelberg_tpanum.hex
--- kinesis/readme.md
+++ kinesis/readme.md
@@ -1,8 +1,16 @@
 # Kinesis
+All kinesis firmware use Vial now, and the Vial config is within `tpanum.vil`.
 Installing the firmware can be done using the teensy-loader, with the following command:
 
+## Flashing teensy board
+
 ``` shell
-sudo teensy-loader-cli -w -v -mmcu=TEENSY2PP kinesis.tpanum*.hex
+sudo teensy-loader-cli -w -v -mmcu=TEENSY2PP kinesis_default_vial.hex
 ```
 
 Now, press the reset button on the keyboard.
+
+## Flashing Blackpill
+```shell
+sudo dfu-util -d 0483:df11 -a 0 -s 0x08000000:leave -D kinesis_blackpill_default.bin
+ ```
